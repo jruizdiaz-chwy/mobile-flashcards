b5526bc3b8fc4ae2d5d6f1ce87689fc2dc8fa196
julianrd <julian.rd7@gmail.com>
2017-12-26 12:21:55 -0300

    fix: Update question reducer initial state

--- reducers/question.js
+++ reducers/question.js
@@ -4,37 +4,37 @@ const initialState = {
       id: 1,
       deck: 'React',
       question: 'What is React?',
-      answer: 'A JavaScript library for reusable components'
+      answer: 'A JavaScript library for reusable components',
     },
     2: {
       id: 2,
       deck: 'React',
       question: 'In what language you write components?',
-      answer: 'JSX'
+      answer: 'JSX',
     },
     3: {
       id: 3,
       deck: 'JavaScript',
       question: 'Is JavaScript strongly-typed?',
-      answer: 'No'
+      answer: 'No',
     },
     4: {
       id: 4,
       deck: 'JavaScript',
       question: 'Does it have something to do with Java?',
-      answer: 'No'
+      answer: 'No',
     },
     5:{
       id: 5,
       deck: 'React',
       question: 'What method is mandatory in every component?',
-      answer: 'render'
+      answer: 'render',
     },
     6: {
       id: 6,
       deck: 'JavaScript',
       question: 'What is a closure?',
-      answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      answer: 'The combination of a function and the lexical environment within which that function was declared.',
     }
   },
   allIds: [1, 2, 3, 4, 5, 6]
